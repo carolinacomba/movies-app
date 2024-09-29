9981ce0afa2de829ea7c457821fb765e04859e38
carolinacomba <113875@tecnicatura.frc.utn.edu.ar>
2024-09-29 13:36:53 -0300

    btn add movie, routes, .gitignore

--- frontend/pelis-app/src/app/app.routes.ts
+++ frontend/pelis-app/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AddMovieComponent } from './components/add-movie/add-movie.component';
+import { ListMoviesComponent } from './components/list-movies/list-movies.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'movies', component: ListMoviesComponent },
+    { path: 'movies/add', component: AddMovieComponent },
+    { path: '**', redirectTo: 'movies' }
+];
